ab461ed3cb31f7e1dc5f4348ae557bb3d4efb307
Steven Pratama <steven.pratama@shopee.com>
2021-12-15 15:43:19 +0800

    add drive functions

--- google_service.py
+++ google_service.py
@@ -16,12 +16,13 @@ import traceback
 import string
 import getpass
 
-HOME_PATH = '/ldap_home/{}/chat_keywords_screening/keyword-screening/'.format(getpass.getuser())
+# HOME_PATH = '/ldap_home/{}/chat_keywords_screening/keyword-screening/'.format(getpass.getuser())
+HOME_PATH = './'
 TOKEN_PATH = HOME_PATH + 'token.json'
 CREDENTIAL_PATH = HOME_PATH + 'credentials.json'
 SCOPES=['https://www.googleapis.com/auth/spreadsheets','https://mail.google.com/','https://www.googleapis.com/auth/drive']
 
-assert os.path.exists(TOKEN_PATH), "could not find %s" % (TOKEN_PATH)
+# assert os.path.exists(TOKEN_PATH), "could not find %s" % (TOKEN_PATH)
 assert os.path.exists(CREDENTIAL_PATH), "could not find %s" % (CREDENTIAL_PATH)
 
 class GService:
@@ -393,6 +394,14 @@ class GService:
 
         print('File ID:', gdrive_file.get('id'))
         return gdrive_file
+    
+    def list_files(self, gdrive_folder_id: str) -> list:
+        service = self.drive_service
+        response = service.files().list(
+            q="'{}' in parents".format(gdrive_folder_id)
+        ).execute()
+
+        return response.get('files', [])
 
 def df_to_value_range(df: pd.DataFrame, include_header=True) -> List[List[str]]:
     df.fillna('', inplace=True)
@@ -420,62 +429,6 @@ def numeric_col_to_letter_col(col_index: int):
 # Testing sheet: https://docs.google.com/spreadsheets/d/1CH5nkNA5-zeeA3wYuU0WRF6Fg4zxg2iJoL3Ti_HGbXc/
 
 if __name__ == "__main__":
-    pass
-    gsheet_id = '1CH5nkNA5-zeeA3wYuU0WRF6Fg4zxg2iJoL3Ti_HGbXc'
-    g_service = GService()
-    
-    # Get the names of sheet in the gsheet
-    sheet_names = g_service.get_sheet_names(gsheet_id)
-
-    # Adding sheet
-    g_service.add_sheet(gsheet_id, 'Sheet1')
-
-    # Writing pandas df to existing sheet
-    df = pd.DataFrame({'column_name':['hello world']})
-    g_service.write_google_sheet(gsheet_id, 'Sheet1!A1', df_to_value_range(df))
-
-    # Writing to existing sheet
-    g_service.write_google_sheet(gsheet_id, 'Sheet1!A1', [['A1','B1'],['A2','B2']])
-
-    # Appending to existing sheet
-    g_service.append_google_sheet(gsheet_id, 'Sheet1!A1', [['A3','B3']])
-
-    # Appending dataframe to existing sheet
-    g_service.append_google_sheet(gsheet_id, 'Sheet1!A1', df_to_value_range(df, include_header=False))
-
-    # Reading gsheet into pandas df
-    df = g_service.read_google_sheet(gsheet_id, 'Sheet1!A1:B3')
-
-    # Get sheet dimension
-    row_count, column_count = g_service.get_sheet_dimension(gsheet_id, 'Sheet1')
-
-    # Get sheet_id
-    sheet_id = g_service.get_sheet_id(gsheet_id, 'Sheet1')
-    print(sheet_id)
-
-    # Add dropdown validation to cells
-    g_service.add_dropdown_validation(
-        gsheet_id = gsheet_id,
-        sheet_id = sheet_id,
-        start_col = 0,
-        end_col = 2,
-        start_row = 0,
-        end_row = 2,
-        values = ['Normal','Freeze','Ban'],
-        input_msg = 'Please select an action',
-        strict = False
-    )
-
-    # Delete rows
-    g_service.delete_row(gsheet_id, sheet_id, 0, 2)
-
-    # Clear sheet
-    g_service.clear_google_sheet(gsheet_id, 'Sheet1!A1:B3')
-
-    # Delete sheet
-    g_service.delete_sheet(gsheet_id, sheet_id)
-
-    # Converting column number to alphabet
-    column_number = 26
-    column_alphabet = numeric_col_to_letter_col(column_number)
-    print(column_alphabet)
+    gs = GService()
+    g_files = gs.list_files('1OyGEzGwnwsWdvQM9SlDkMHOnLnahpy_y')
+    print(g_files)
